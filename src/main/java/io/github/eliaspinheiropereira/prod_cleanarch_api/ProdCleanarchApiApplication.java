d5cdf9cbecda73681848731312c2875e52031fcd
Elias P. Pereira <eliaspinheiropereiraa@gmail.com>
2026-04-29 19:42:13 -0300

    iniciando projeto

---
+++ src/main/java/io/github/eliaspinheiropereira/prod_cleanarch_api/ProdCleanarchApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.eliaspinheiropereira.prod_cleanarch_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdCleanarchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdCleanarchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/eliaspinheiropereira/prod_cleanarch_api/ProdCleanarchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eliaspinheiropereira.prod_cleanarch_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdCleanarchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
